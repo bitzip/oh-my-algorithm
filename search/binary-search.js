a6ec2634e0d152a455a6d3643c783f99e35599dc
Kelly Apollo <nagisa.fff@gmail.com>
2017-10-16 10:52:05 -0500

    易懂格式化

--- search/binary-search.js
+++ search/binary-search.js
@@ -2,16 +2,17 @@
    * https://en.wikipedia.org/wiki/Binary_search_algorithm
    */
 
-
 var binarySearch
   , test
 
 test = function (fn) {
-  var arr = [1,2,3,4,5,6,7,8,9,10]
+  var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   console.log(fn(arr, 5) === 4)
   console.log(fn(arr, 0) === -1)
+
   list = [1]
   console.log(fn(arr, 1) === 0)
+
   list = [1,2]
   console.log(fn(arr, 2) === 1)
 }
@@ -22,13 +23,19 @@ binarySearch = function (arr, item) {
   , right = arr.length - 1
   , pivot
 
-  while (pivot = Math.floor((left + right)/2), arr[pivot] != item && left <= right) {
-    if (arr[pivot] < item) {
+  while (left <= right) {
+    pivot = Math.floor((left + right) / 2)
+    if (arr[pivot] === item) {
+      return pivot
+    }
+    else if (arr[pivot] < item) {
       left = pivot + 1
     } else {
       right = pivot - 1
     }
   }
-  return left <= right ? pivot : -1
+  return -1
 }
+
 test(binarySearch)
+
